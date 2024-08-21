557ccc8eaab7850742c1a19e54a3c6e78e58427e
CrisleiKeli <crislei.jenuino@etec.sp.gov>
2024-08-21 16:17:18 -0300

    Com correção de erro e do código

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
-let heroina = "ChryslleyKelly"
-let xp = 10.0000;
+let heroina = "ChryslleyKelly";
+let xp = 10.001;
 
-if (xp > 1.000) {
+if (xp < 1.000) {
     xp = "Ferro";
-} else if (xp >= 1.001 && xp <= 2.000){
+} else if (xp >= 1.000 && xp <= 2.000) {
     xp = "Bronze";
-} 
-
-if (xp >= 1.001 && xp <= 2.000){
+} else if (xp >= 2.001 && xp <= 5.000) {
     xp = "Prata";
-} else if (xp >= 2.001 && xp <= 5.000){
+} else if (xp >= 5.001 && xp <= 7.000) {
     xp = "Ouro";
-} 
-
-if (xp >= 5.001 && xp <= 7.000){
-    xp = "Ouro";
-} else if (xp >= 7.001 && xp <= 8.000){
+} else if (xp >= 7.001 && xp <= 8.000) {
     xp = "Platina";
-} 
-
-if (xp >= 8.001 && xp <= 9.000){
+} else if (xp >= 8.001 && xp <= 9.000) {
     xp = "Ascendente";
-} else if (xp >= 9.001 && xp <= 10.000){
+} else if (xp >= 9.001 && xp <= 10.000) {
     xp = "Imortal";
-} 
-
-if (xp = 10.000){
+} else if (xp >= 10.001) {
     xp = "Radiante";
-} 
+}
 
 console.log("A Heroína", heroina, "está no nível", xp);
+
