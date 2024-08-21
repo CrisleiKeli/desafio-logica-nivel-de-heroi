4249b9990fb59a07995e391f6c87fe67fc0b8c46
CrisleiKeli <crislei.jenuino@etec.sp.gov>
2024-08-21 17:06:23 -0300

    melhorias no código e acrescentei a variável nivel que estava faltando

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 let heroina = "ChryslleyKelly";
-let xp = 10.001;
+let xp = 10001;
+let nivel = "";
 
-if (xp < 1.000) {
-    xp = "Ferro";
-} else if (xp >= 1.000 && xp <= 2.000) {
-    xp = "Bronze";
-} else if (xp >= 2.001 && xp <= 5.000) {
-    xp = "Prata";
-} else if (xp >= 5.001 && xp <= 7.000) {
-    xp = "Ouro";
-} else if (xp >= 7.001 && xp <= 8.000) {
-    xp = "Platina";
-} else if (xp >= 8.001 && xp <= 9.000) {
-    xp = "Ascendente";
-} else if (xp >= 9.001 && xp <= 10.000) {
-    xp = "Imortal";
-} else if (xp >= 10.001) {
-    xp = "Radiante";
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1000 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
 }
 
-console.log("A Heroína", heroina, "está no nível", xp);
+console.log(`A Heroina ${heroina} é nível ${nivel} com ${xp} de xp.`);
 
